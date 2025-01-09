3466115ff0e0d02a1ca5bc8ecc7b71cfe995d94e
B1NUR4 <binuraofficial32@gmail.com>
2025-01-09 22:42:16 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Country from "./Country";
 import Product from "./Product";
-
 function App() {
-
-  let [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  let [countries, setCountries] = useState(
-    [
-      { name: "India", Capital: "New Delhi", population: "1.4B" },
-      { name: "Sri Lanka", Capital: "Sri Jayawaranapura", population: "20M" }
-    ]
-  )
-
-  function loadNewCountries() {
-    setCountries(
-      [
-        { name: "USA", Capital: "Washington D.C.", population: "331.7M" },
-        { name: "China", Capital: "Beijing", population: "1.4B" }
-      ]
-    )
-  }
-
   return (
     <div>
-      <h1>Main Component</h1>
-
-      <button onClick={loadNewCountries}>Load New Countries </button>
-
-      <button onClick={() => {
-        setIsLoggedIn(true);
-      }}>Login</button>
-
-      {
-        isLoggedIn == false ?
-          (<h1>Please Login</h1>) :
-          countries.map((country) => {
-            return (
-              <Country name={country.name} Capital={country.Capital} population={country.population} key={country.name} />
-            )
-          })
-      }
+      <h1>UseEffect Demo</h1>
+      <Product/>
     </div>
   );
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,24 @@
-function Product(props) {
-    let { name, price, specs } = props;
-    
+import { useEffect, useState } from "react"
+
+function Product() {
+
+    let [name, setName] = useState("Samsung Galaxy A21s");
+    let [price, setPrice] = useState(28000);
+
+    useEffect(()=>{
+        console.log("Component Mounted");
+    })
+
     return (
         <div className="product">
-            <h3>{name}</h3>
-            <p>{price}</p>
-            <p>{specs.ram}</p>
-            <p>{specs.size}</p>
+            <h1>{name}</h1>
+            <p>price : {price}</p>
+            <button onClick={()=>{
+                setName("Iphone XR"),
+                setPrice("71000")
+            }}>Change</button>
         </div>
     )
 }
 
-export default Product;
+export default Product
