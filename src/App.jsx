da13723406633e454e4fbc6f746defc1f6cbefcc
B1NUR4 <binuraofficial32@gmail.com>
2025-01-09 15:36:42 +0530

    conditiona statement using

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Product from "./Product";
 
 function App() {
 
+  let [isLoggedIn, setIsLoggedIn] = useState(false);
+
   let [countries, setCountries] = useState(
     [
       { name: "India", Capital: "New Delhi", population: "1.4B" },
-      { name: "Sri Lanka", Capital: "Sri Jayawaranapura", population: "20M" },
-      { name: "France", Capital: "Paris", population: "2.5B" },
-      { name: "Australia", Capital: "Canaberra", population: "67.5M" }
+      { name: "Sri Lanka", Capital: "Sri Jayawaranapura", population: "20M" }
     ]
   )
 
@@ -18,8 +18,6 @@ function App() {
     setCountries(
       [
         { name: "USA", Capital: "Washington D.C.", population: "331.7M" },
-        { name: "Canada", Capital: "Ottawa", population: "37.6M" },
-        { name: "Brazil", Capital: "Brasília", population: "212.5M" },
         { name: "China", Capital: "Beijing", population: "1.4B" }
       ]
     )
@@ -31,16 +29,21 @@ function App() {
 
       <button onClick={loadNewCountries}>Load New Countries </button>
 
+      <button onClick={() => {
+        setIsLoggedIn(true);
+      }}>Login</button>
+
       {
-        countries.map((country) => {
-          return (
-            <Country name={country.name} Capital={country.Capital} population={country.population} key={country.name} />
-          )
-        })
+        isLoggedIn == false ?
+          (<h1>Please Login</h1>) :
+          countries.map((country) => {
+            return (
+              <Country name={country.name} Capital={country.Capital} population={country.population} key={country.name} />
+            )
+          })
       }
     </div>
   );
-  
 }
 
 export default App;
