cdccf769352f9e2ffe871ef8ba368fde380b9fb4
B1NUR4 <binuraofficial32@gmail.com>
2025-01-11 00:23:53 +0530

    multiple input fields data gathering

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,25 @@ import { useState } from "react";
 import "./App.css";
 function App() {
 
-  let [name,setName] = useState("");
-  let [email,setEmail] = useState("");
+  let [details, setDeatils] = useState({})
 
   function handleInput(event) {
-    setName(event.target.value);
-  }
+    console.log(event);
 
-  function handleEmail(event) {
-    setEmail(event.target.value);
+    setDeatils((prevObj) => {
+      return { ...prevObj, [event.target.name]: event.target.value }
+    })
+    console.log(details);
+
   }
 
   return (
     <div className="data-form">
 
-      <button onClick={()=>{
-        setName("Binura")
-      }}>Change Name</button>
+      <input type="text" name='name' onChange={handleInput} placeholder="Enter Name" /> <br />
 
-      {/* two way data Binding */}
-      <input type="text" onChange={handleInput} placeholder="Enter Name" value={name}/>
+      <input type="text" name='email' onChange={handleInput} placeholder="Enter Email" />
 
-      {/* one way data Binding */}
-      <input type="text" onChange={handleEmail} placeholder="Enter Email"/>
-
-      <h1>{name}</h1>
-      <h1>{email}</h1>
-      
     </div>
   )
 }
