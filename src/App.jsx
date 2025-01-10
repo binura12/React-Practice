e0cd15257129ce16baa24a20853061a5afed028c
B1NUR4 <binuraofficial32@gmail.com>
2025-01-11 00:03:08 +0530

    two way data binding example

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import "./App.css";
 function App() {
 
   let [name,setName] = useState("");
+  let [email,setEmail] = useState("");
 
   function handleInput(event) {
     setName(event.target.value);
   }
 
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+
   return (
     <div className="data-form">
-      <input type="text" onChange={handleInput} placeholder="Enter Name"/>
+
+      <button onClick={()=>{
+        setName("Binura")
+      }}>Change Name</button>
+
+      {/* two way data Binding */}
+      <input type="text" onChange={handleInput} placeholder="Enter Name" value={name}/>
+
+      {/* one way data Binding */}
+      <input type="text" onChange={handleEmail} placeholder="Enter Email"/>
+
       <h1>{name}</h1>
+      <h1>{email}</h1>
+      
     </div>
   )
 }
