bdd24eaee8c0592294f85f1a15354616f482d6d5
B1NUR4 <binuraofficial32@gmail.com>
2025-01-11 00:51:30 +0530

    two data binding complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import { useState } from "react";
 import "./App.css";
 function App() {
 
-  let [details, setDeatils] = useState({})
+  let [details, setDeatils] = useState({
+    name:"",
+    email:""
+  })
 
   function handleInput(event) {
-    console.log(event);
-
     setDeatils((prevObj) => {
       return { ...prevObj, [event.target.name]: event.target.value }
     })
-    console.log(details);
 
+  }
+
+  function handleSubmit() {
+    console.log(details);
+    
   }
 
   return (
     <div className="data-form">
 
-      <input type="text" name='name' onChange={handleInput} placeholder="Enter Name" /> <br />
+      <input type="text" name='name' onChange={handleInput} placeholder="Enter Name" value={details.name}/> <br />
 
-      <input type="text" name='email' onChange={handleInput} placeholder="Enter Email" />
+      <input type="text" name='email' onChange={handleInput} placeholder="Enter Email" value={details.email}/>
+      
+      <button onClick={handleSubmit}>Submit</button>
 
     </div>
   )
