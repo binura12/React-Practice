3e49f81e01307b936d49f7f17b633968d1edb29c
B1NUR4 <binuraofficial32@gmail.com>
2025-01-18 22:04:41 +0530

    routed by using Link tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import "./App.css";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import About from "./Components/About";
 import Product from "./Components/Product";
 import Service from "./Components/Service";
+import Home from "./Components/Home";
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/product">Product</Link>
+            </li>
+            <li>
+              <Link to="/service">Service</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
-          <Route path="/" element={<About/>}/>
-          <Route path="/product" element={<Product/>}/>
-          <Route path="/service" element={<Service/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/service" element={<Service />} />
         </Routes>
       </BrowserRouter>
     </div>
