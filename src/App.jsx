cc85a71525a6a04e83564c399e0cc83581a64162
B1NUR4 <binuraofficial32@gmail.com>
2025-01-18 20:19:12 +0530

    react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from "react";
 import "./App.css";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import About from "./Components/About";
+import Product from "./Components/Product";
+import Service from "./Components/Service";
 function App() {
-
-  let [details, setDeatils] = useState({
-    name:"",
-    email:""
-  })
-
-  function handleInput(event) {
-    setDeatils((prevObj) => {
-      return { ...prevObj, [event.target.name]: event.target.value }
-    })
-
-  }
-
-  function handleSubmit() {
-    console.log(details);
-    
-  }
-
   return (
-    <div className="data-form">
-
-      <input type="text" name='name' onChange={handleInput} placeholder="Enter Name" value={details.name}/> <br />
-
-      <input type="text" name='email' onChange={handleInput} placeholder="Enter Email" value={details.email}/>
-      
-      <button onClick={handleSubmit}>Submit</button>
-
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<About/>}/>
+          <Route path="/product" element={<Product/>}/>
+          <Route path="/service" element={<Service/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
