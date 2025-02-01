80f280fd7244c6bbf6c9d809cf11fc3f70583e32
B1NUR4 <binuraofficial32@gmail.com>
2025-02-01 23:34:22 +0530

    import and display images on react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
-import Body from './Components/Body/Body'
-import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <>
       <div id="wrapper">
-        <Header name="Home"/>
-        <Header name="About"/>
-        <Header name="Contact"/>
-        <Header name="Menu"/>
-        <Body>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, adipisci sint dolore iste animi optio laborum libero provident. Quibusdam eum voluptatem inventore ipsa numquam quaerat provident cumque, explicabo totam odit.</p>
-        </Body>
-        <Footer/>
+        <Header/>
       </div>
     </>
   )
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import LogoImage from '../../assets/react.svg'
 
 function Header(props) {
-    return (
-        <>
-            <h3 id="header">{props.name}</h3>
-        </>
-    )
+  return (
+    <>
+      <img src="vite.svg" alt="Vite Logo" />
+      <img src={LogoImage} alt="React Logo" />
+    </>
+  );
 }
 
-export default Header
+export default Header;
